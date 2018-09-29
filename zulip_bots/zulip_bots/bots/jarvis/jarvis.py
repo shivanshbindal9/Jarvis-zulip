44b726f6ec7b628f4aaa755a6250881f8a5e6f0e
shivansh bindal <shivanshbindal603@gmail.com>
2018-09-29 20:58:26 +0530

    adds teach_me

--- zulip_bots/zulip_bots/bots/jarvis/jarvis.py
+++ zulip_bots/zulip_bots/bots/jarvis/jarvis.py
@@ -35,7 +35,13 @@ class JarvisHandler(object):
             bot_response = self.get_bot_joke_response(message,bot_handler)
         if first_word == "wikipedia":
             bot_response = self.get_bot_wiki_response(message, bot_handler)
+        if first_word == "teach_me":
+            bot_response = self.get_bot_teach_response(message, bot_handler)
+        else:
+            bot_response = self.get_bot_reply(message, bot_handler)
         bot_handler.send_reply(message, bot_response)
+
+
 
     def get_bot_wiki_response(self, message: Dict[str, str], bot_handler: Any) -> Optional[str]:
         '''This function returns the URLs of the requested topic.'''
@@ -112,7 +118,56 @@ class JarvisHandler(object):
         return new_content
 
 
-    #def get_bot_news(self, message, bot_handler: Any) -> Optional[str]:
+    def get_bot_teach_response(self, message, bot_handler: Any) -> Optional[str]:
+        '''This function returns the URLs of the requested topic.'''
+
+        help_text = 'Please enter your search term after {}'
+
+        # Checking if the link exists.
+        query = message['content'][8:]
+        if query == '':
+            return help_text.format(bot_handler.identity().mention)
+
+        query_wiki_url = 'https://www.googleapis.com/customsearch/v1'
+        query_wiki_params = dict(
+            key = 'AIzaSyDWus4C1ykIrL3q7uIYB1MCTIwdM5wfQDo',
+            cx = '004985854750889686468:okdojnlvqsw',
+            q=query,
+           
+        )
+        try:
+            data = requests.get(query_wiki_url, params=query_wiki_params)
+
+        except requests.exceptions.RequestException:
+            logging.error('broken link')
+            return 'Uh-Oh ! Sorry ,couldn\'t process the request right now.:slightly_frowning_face:\n' \
+                   'Please try again later.'
+
+        # Checking if the bot accessed the link.
+        if data.status_code != 200:
+            logging.error('Page not found.')
+            return 'Uh-Oh ! Sorry ,couldn\'t process the request right now.:slightly_frowning_face:\n' \
+                   'Please try again later.'
+
+        new_content = 'For search term:' + query + '\n'
+
+        # Checking if there is content for the searched term
+        if data.json()['queries']['request'][0]['count'] == 0:
+            new_content = 'I am sorry. The search term you provided is not found :slightly_frowning_face:'
+        else:
+            for i in range(min(3, data.json()['queries']['request'][0]['count'])):
+                result_title = data.json()['items'][i]['title']
+                url = data.json()['items'][i]['link']
+                snippet = data.json()['items'][i]['snippet']
+
+                new_content += 'Title' + ':' + result_title + '\n' + 'Link' + ':' + url + '\n' \
+                 + 'Description:' + snippet +  '\n' + '\n'
+        return new_content
+
+    def get_bot_reply(self, message, bot_handler: Any) -> Optional[str]:
+        
+        new_content = "It is not a valid query \n please check '@jarvis help'"
+        return new_content
 
 
 
