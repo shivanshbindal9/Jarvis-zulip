641699c1956ec6ac1bc7c33875da27f384671b2e
shivansh bindal <shivanshbindal603@gmail.com>
2018-09-29 22:18:19 +0530

    adds help

--- zulip_bots/zulip_bots/bots/jarvis/jarvis.py
+++ zulip_bots/zulip_bots/bots/jarvis/jarvis.py
@@ -33,13 +33,15 @@ class JarvisHandler(object):
         first_word = message['content'].split(' ', 1)[0]
         if first_word == "joke":
             bot_response = self.get_bot_joke_response(message,bot_handler)
-        if first_word == "wikipedia":
+        elif first_word == "wikipedia":
             bot_response = self.get_bot_wiki_response(message, bot_handler)
-        if first_word == "teach_me":
+        elif first_word == "teach_me":
             bot_response = self.get_bot_teach_response(message, bot_handler)
-        if first_word == "news":
+        elif first_word == "news":
             bot_response = self.get_bot_news(message,bot_handler)
-         else:
+        elif first_word == "help":
+            bot_response = self.get_bot_help_response(message,bot_handler)
+        else:
             bot_response = self.get_bot_reply(message, bot_handler)
 
         bot_handler.send_reply(message, bot_response)
@@ -141,8 +143,28 @@ class JarvisHandler(object):
             #logging.error('broken link')
             return 'Uh-Oh ! Sorry ,couldn\'t process the request right now. :slightly_frowning_face:\n' \
                    'Please try again later.'
+             # Checking if there is content for the searched term
+        if data.status_code != 200:
+            return 'Uh-Oh ! Sorry ,couldn\'t process the request right now.:slightly_frowning_face:\n' \
+                   'Please try again later. '+str(data.status_code)
 
-<<<<<<< HEAD
+        new_content = 'For search term:' + query + '\n'
+        if len(data.json()['articles'])== 0:
+            new_content = 'I am sorry. The search term you provided is not found :slightly_frowning_face:'
+
+        else:
+            for i in range(min(3,len(data.json()['articles']))):
+                author = data.json()['articles'][i]['author']
+                title = data.json()['articles'][i]['title']
+                description = data.json()['articles'][i]['description']
+                url = data.json()['articles'][i]['url']
+                if author is None:
+                    new_content += str(
+                        i + 1) + ': Title: ' + title + '\n   ' + description + '\n (' + url + ') \n'
+                else:
+                    new_content += str(i+1) + ': Author: '+author+'\n   Title: '+title+'\n   '+description+'\n ('+url+') \n'
+        return new_content
+
     def get_bot_teach_response(self, message, bot_handler: Any) -> Optional[str]:
         '''This function returns the URLs of the requested topic.'''
 
@@ -189,35 +211,14 @@ class JarvisHandler(object):
                  + 'Description:' + snippet +  '\n' + '\n'
         return new_content
 
+    def get_bot_help_response(self, message, bot_handler:Any) -> Optional[str]:
+        new_content = "A list of functionalities is as given below: \n @Jarvis wikipedia <Query> \n @Jarvis teach_me <Topic Name> \n @Jarvis news <Topic> \n @Jarvis joke"
+        return new_content
+
     def get_bot_reply(self, message, bot_handler: Any) -> Optional[str]:
         
         new_content = "It is not a valid query \n please check '@jarvis help'"
-        return new_content
-=======
-        # Checking if the bot accessed the link.
-        if data.status_code != 200:
-            return 'Uh-Oh ! Sorry ,couldn\'t process the request right now.:slightly_frowning_face:\n' \
-                   'Please try again later. '+str(data.status_code)
-
-        new_content = 'For search term:' + query + '\n'
->>>>>>> cec5a1e9364662e9c061222385b6cc96f2845569
-
-        # Checking if there is content for the searched term
-        if len(data.json()['articles'])== 0:
-            new_content = 'I am sorry. The search term you provided is not found :slightly_frowning_face:'
-
-        else:
-            for i in range(min(3,len(data.json()['articles']))):
-                author = data.json()['articles'][i]['author']
-                title = data.json()['articles'][i]['title']
-                description = data.json()['articles'][i]['description']
-                url = data.json()['articles'][i]['url']
-                if author is None:
-                    new_content += str(
-                        i + 1) + ': Title: ' + title + '\n   ' + description + '\n (' + url + ') \n'
-                else:
-                    new_content += str(i+1) + ': Author: '+author+'\n   Title: '+title+'\n   '+description+'\n ('+url+') \n'
-        return new_content
+        return new_content      
 
 
 handler_class = JarvisHandler
