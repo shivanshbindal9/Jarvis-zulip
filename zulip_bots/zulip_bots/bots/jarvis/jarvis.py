a44b1b4d09306c150af8aa0aeca0580360dca26c
Saurabh Singh <saurabhsinghmay99@gmail.com>
2018-09-29 19:04:35 +0530

    News added

--- zulip_bots/zulip_bots/bots/jarvis/jarvis.py
+++ zulip_bots/zulip_bots/bots/jarvis/jarvis.py
@@ -31,10 +31,11 @@ class JarvisHandler(object):
     def handle_message(self, message: Dict[str, str], bot_handler: Any) -> None:
 
         first_word = message['content'].split(' ', 1)[0]
-        #if first_word == "news":
-            #bot_response = self.get_bot_news(message,bot_handler)
+
         if first_word == "wikipedia":
             bot_response = self.get_bot_wiki_response(message, bot_handler)
+        elif first_word == "news":
+            bot_response = self.get_bot_news(message,bot_handler)
         bot_handler.send_reply(message, bot_response)
 
     def get_bot_wiki_response(self, message: Dict[str, str], bot_handler: Any) -> Optional[str]:
@@ -82,9 +83,51 @@ class JarvisHandler(object):
 
     #def get_bot_joke_response(self, message: Dict[str, str], bot_handler: Any) -> Optional[str]:
 
+    def get_bot_news(self, message, bot_handler: Any) -> Optional[str]:
+        help_text = 'Please enter your search term after {}'
 
-    #def get_bot_news(self, message, bot_handler: Any) -> Optional[str]:
+        # Checking if the link exists.
+        query = message['content'][5:]
+        if query == '':
+            return help_text.format(bot_handler.identity().mention)
 
+        query_news_url = 'https://newsapi.org/v2/everything?'
+        query_news_params = dict(
+            apiKey='e7682c2d3cd64221984d798bdc9dff4b',
+            language='en',
+            pageSize=3,
+            q=query
+        )
+        try:
+            data = requests.get(query_news_url,query_news_params)
+        except requests.exceptions.RequestException:
+            #logging.error('broken link')
+            return 'Uh-Oh ! Sorry ,couldn\'t process the request right now. :slightly_frowning_face:\n' \
+                   'Please try again later.'
+
+        # Checking if the bot accessed the link.
+        if data.status_code != 200:
+            return 'Uh-Oh ! Sorry ,couldn\'t process the request right now.:slightly_frowning_face:\n' \
+                   'Please try again later. '+str(data.status_code)
+
+        new_content = 'For search term:' + query + '\n'
+
+        # Checking if there is content for the searched term
+        if len(data.json()['articles'])== 0:
+            new_content = 'I am sorry. The search term you provided is not found :slightly_frowning_face:'
+
+        else:
+            for i in range(min(3,len(data.json()['articles']))):
+                author = data.json()['articles'][i]['author']
+                title = data.json()['articles'][i]['title']
+                description = data.json()['articles'][i]['description']
+                url = data.json()['articles'][i]['url']
+                if author is None:
+                    new_content += str(
+                        i + 1) + ': Title: ' + title + '\n   ' + description + '\n (' + url + ') \n'
+                else:
+                    new_content += str(i+1) + ': Author: '+author+'\n   Title: '+title+'\n   '+description+'\n ('+url+') \n'
+        return new_content
 
 
 handler_class = JarvisHandler
